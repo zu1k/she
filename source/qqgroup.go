3ba32d57a939a2761629f64e21989612fddc3d7d
zu1k <42370281+zu1k@users.noreply.github.com>
2020-03-05 13:02:46 +0800

    Add: func searchMemberByQQNum

--- source/qqgroup.go
+++ source/qqgroup.go
@@ -2,6 +2,7 @@ package source
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mssql"
@@ -29,4 +30,58 @@ func (q *qqGroup) GetName() string {
 
 func (q *qqGroup) Search(key interface{}) (result []Result) {
 	return nil
+}
+
+type group struct {
+	Id         int    `gorm:"column:Id"`
+	GroupNum   int    `gorm:"column:GroupNum"`
+	Mast       int    `gorm:"column:Mast"`
+	CreateDate string `gorm:"column:CreateDate"`
+	Title      string `gorm:"column:Title"`
+	Class      string `gorm:"column:Class"`
+	Summary    string `gorm:"column:Summary"`
+}
+
+func (group) TableName() string {
+	return "Group"
+}
+
+type member struct {
+	Id       int    `gorm:"column:Id"`
+	QQNum    int    `gorm:"column:QQNum"`
+	Nick     string `gorm:"column:Nick"`
+	Age      string `gorm:"column:Age"`
+	Gender   int    `gorm:"column:Gender"`
+	Auth     int    `gorm:"column:Auth"`
+	GroupNum int    `gorm:"column:GroupNum"`
+}
+
+func (member) TableName() string {
+	return "Member"
+}
+
+func (m member) String() string {
+	return m.Nick
+}
+
+func (q *qqGroup) searchMemberByQQNum(qqNum int) (results []Result) {
+	var memberRes []member
+	q.db.Where("QQNum=?", qqNum).Find(&memberRes)
+	for _, m := range memberRes {
+		result := Result{
+			Score: 1,
+			Hit:   strconv.Itoa(qqNum),
+			Text:  m.String(),
+		}
+		results = append(results, result)
+	}
+	return
+}
+
+func (q *qqGroup) searchMemberByGroupNum(groupNum int) {
+
+}
+
+func (q *qqGroup) searchGroupByGroupNum(groupNum int) {
+
 }
