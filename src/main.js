4f06dedee471ed736b2843e4c66f56754c37ea59
zu1k <42370281+zu1k@users.noreply.github.com>
2020-03-06 18:11:53 +0800

    Feature: simple search

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ZeitUI from '@zeit-ui/vue'
+import '@zeit-ui/vue/dist/zeit-ui.css' // require style
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+Vue.use(ZeitUI)
+
+// ZeitUI.theme.enableLight()
+// ZeitUI.theme.enableDark()
 
 new Vue({
   render: h => h(App),
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  publicPath: './', // 相对于 HTML 页面（目录相同）
+}
